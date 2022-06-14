bd272467066a91fb348a97d69fd6d57b94a35d70
Joao Oliveira <joaogetknow@gmail.com>
2022-06-14 15:54:27 -0300

    Initialize controller

--- api-rick-and-morty/src/main/java/com/api/apirickandmorty/controller/RickAndMortyController.java
+++ api-rick-and-morty/src/main/java/com/api/apirickandmorty/controller/RickAndMortyController.java
@@ -1,2 +1,12 @@
-package com.api.apirickandmorty.controller;public class RickAndMortyController {
+package com.api.apirickandmorty.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/webclient")
+public class RickAndMortyController {
+
 }
