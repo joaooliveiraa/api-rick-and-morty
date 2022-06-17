2bd1a290a7e00243fc5e5cef175a0d2af9adee70
Joao Oliveira <joaogetknow@gmail.com>
2022-06-17 17:01:52 -0300

    CharacterResponse

--- api-rick-and-morty/src/main/java/com/api/apirickandmorty/response/CharacterResponse.java
+++ api-rick-and-morty/src/main/java/com/api/apirickandmorty/response/CharacterResponse.java
@@ -1,4 +1,12 @@
 package com.api.apirickandmorty.response;
 
+import java.util.List;
+
 public class CharacterResponse {
+    private String id;
+    private String name;
+    private String status;
+    private String species;
+    private String image;
+    private List<String> episode;
 }
